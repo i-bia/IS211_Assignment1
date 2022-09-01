db4cc667ac1954f074542bd34116da69ab3e7abf
i-bia <ibiyya@icloud.com>
2022-08-31 23:04:51 -0400

    commit changes

---
+++ assignment1_part1.py
@@ -0,0 +1,15 @@
+def list_divide (numbers, divide):
+    pass
+
+class ListDivideException ():
+    pass
+
+def test_list_divide():
+    assert list_divide([1,2,3,4,5]) == 2
+    assert list_divide([2,4,6,8,10]) == 5
+    assert list_divide([30,54,63,98,100], divide=10) == 2
+    assert list_divide([]) == 0
+    assert list_divide([1,2,3,4,5], 1) == 5
+
+if __name__ == "__main__":
+    test_list_divide()
